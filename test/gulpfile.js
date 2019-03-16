7abe6a18353282f847dc16200b68ba96a26caaf5
hzg <15934567149@163.com>
2019-03-16 11:47:03 +0800

    scss任务

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,2 +1,11 @@
 const gulp = require('gulp');
 const webserver = require('gulp-webserver');
+const gulpSass = require('gulp-sass');
+const minCss = require('gulp-clean-css');
+
+gulp.task('scss', () => {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(gulpSass())
+        .pipe(minCss())
+        .pipe(gulp.dest('./src/css'))
+})
